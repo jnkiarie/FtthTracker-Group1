ffb68e205c13e939a436ebb10e2e793365fbf61a
Jimmy <jimmyn.kiarie@gmail.com>
2022-08-18 23:11:49 +0300

    updated script.js

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -38,28 +38,11 @@ progressPrev.addEventListener("click", () => {
 
 let functionarray = [
   func1 = () => {
-<<<<<<< HEAD
-    const stage1 = "Dear A, Ticket ID: REQ123456 Picked by Safaricom LTD and is being actioned, You will get an update once it progresses"
-=======
     const stage1 = "Dear Customer, Ticket ID: REQ123456 Picked by Safaricom LTD and is being actioned, You will get an update once it progresses"
->>>>>>> b8af53ebe0c36f9a3c8c1194f0b007087e5eb3ee
     document.getElementById("ticket").innerHTML = stage1
   },
 
   func2 = () => {
-<<<<<<< HEAD
-    const stage2 = "Dear A, Ticket ID: REQ123456 Ticket Details Updated, Likoni Road, Viraj Apartments, House Number 48"
-    document.getElementById("ticketdetails").innerHTML = stage2
-  },
-
-  func2 = () => {
-    const stage3 = "Dear A, Ticket ID: REQ123456: Technician B has been assigned your Ticket will attend to your issue"
-    document.getElementById("technician").innerHTML = stage3
-  },
-
-  func2 = () => {
-    const stage4 = "Dear A, Ticket ID: REQ123456: Router Replaced and Configurations Updated, Thank for Choosing Safaricom PLC"
-=======
     const stage2 = "Dear Customer, Ticket ID: REQ123456 Ticket Details Updated, Likoni Road, Viraj Apartments, House Number 48"
     document.getElementById("ticketdetails").innerHTML = stage2
   },
@@ -71,7 +54,6 @@ let functionarray = [
 
   func4 = () => {
     const stage4 = "Dear Customer, Ticket ID: REQ123456: Technician B is on the way to attend to your issue"
->>>>>>> b8af53ebe0c36f9a3c8c1194f0b007087e5eb3ee
     document.getElementById("location").innerHTML = stage4
   },
 ]
@@ -85,18 +67,10 @@ const updateProgress = () => {
 
     if (i < active) {
       step.classList.add("active");
-<<<<<<< HEAD
-
-=======
->>>>>>> b8af53ebe0c36f9a3c8c1194f0b007087e5eb3ee
     }
     else {
       step.classList.remove("active");
     }
-<<<<<<< HEAD
-
-=======
->>>>>>> b8af53ebe0c36f9a3c8c1194f0b007087e5eb3ee
   });
 
   progressBar.style.width = ((active - 1) / (steps.length - 1)) * 100 + "%";
